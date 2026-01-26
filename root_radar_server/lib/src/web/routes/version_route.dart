f9c4f1accfd34fa3a265e545b793d539852a3478
Dorien Van den Abbeele <dorienvandenabbeele@Doriens-MacBook-Air.local>
2026-01-26 13:41:18 -0500

    fix: re-add version template to resolve 500 mapping error

--- root_radar_server/lib/src/web/routes/version_route.dart
+++ root_radar_server/lib/src/web/routes/version_route.dart
@@ -2,10 +2,7 @@ import 'package:serverpod/serverpod.dart';
 import '../../version.dart';
 
 class VersionWidget extends TemplateWidget {
-  VersionWidget() : super(name: 'version');
-  
-  @override
-  String toString() => fullServerVersion;
+  VersionWidget() : super(name: 'version', values: {'fullServerVersion': fullServerVersion});
 }
 
 class VersionRoute extends WidgetRoute {
